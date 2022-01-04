1bafd49dab5c1a955c915faadacd49afa856704e
nguyenbae1712 <34834564+nguyenbae1712@users.noreply.github.com>
2022-01-04 10:34:35 -0500

    UI Update

---
+++ app/src/main/java/com/midterm/educationmanagement/DanhSachLopActivity.java
@@ -0,0 +1,14 @@
+package com.midterm.educationmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DanhSachLopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_sach_lop);
+    }
+}
