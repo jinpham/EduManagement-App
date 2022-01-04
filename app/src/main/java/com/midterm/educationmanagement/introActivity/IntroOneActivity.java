4e5efd56043c4264b23c293d8a9c1d33bcb19f60
nguyenbae1712 <34834564+nguyenbae1712@users.noreply.github.com>
2022-01-04 07:35:30 -0500

    Intro UI Update

---
+++ app/src/main/java/com/midterm/educationmanagement/introActivity/IntroOneActivity.java
@@ -0,0 +1,16 @@
+package com.midterm.educationmanagement.introActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.midterm.educationmanagement.R;
+
+public class IntroOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_one);
+    }
+}
