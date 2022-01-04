a9d0e5448655ad3ef851376ce56f1ab5c784c20c
Vu~ <minhvu30041999@gmail.com>
2022-01-05 00:39:21 +0700

    activities

--- app/src/main/java/com/midterm/educationmanagement/InFoAppActivity.java
+++ app/src/main/java/com/midterm/educationmanagement/InFoAppActivity.java
@@ -3,12 +3,23 @@ package com.midterm.educationmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class InFoAppActivity extends AppCompatActivity {
-
+    RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_in_fo_app);
+        setContentView(R.layout.activity_info_app);
+        relativeLayout=findViewById(R.id.relativel_layout);
+        if(ManagerActivity.isDark==true) {
+            // dark theme is on
+            relativeLayout.setBackgroundColor(getResources().getColor(R.color.black));
+        }
+        else
+        {
+            // light theme is on
+            relativeLayout.setBackgroundColor(getResources().getColor(R.color.white));
+        }
     }
 }
