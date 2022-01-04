aa355a5f8cdb442c147cc7ce92e4e0547ba1d0ad
PhamJin <phamjin99@gmail.com>
2022-01-04 22:24:28 +0700

    Add Student and Class UI

---
+++ app/src/main/java/com/midterm/educationmanagement/ThemLopActivity.java
@@ -0,0 +1,13 @@
+package com.midterm.educationmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThemLopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_them_lop);
+    }
+}
